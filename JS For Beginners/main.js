fd36c6143f349ae7f1f679b4c888b2773a5c6421
ZeKaspijs <kaspars1503@gmail.com>
2025-08-24 18:58:54 +0300

    Async JS Crash Course

--- JS For Beginners/main.js
+++ JS For Beginners/main.js
@@ -1,7 +1,28 @@
-const ul = document.querySelector('.items');
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
 
-// ul.remove();
-// ul.lastElementChild.remove();
-ul.firstElementChild.textContent = 'Hello';
-ul.children[1].innerText = 'Kaspars';
-ul.lastElementChild.innerHTML = '<h1>Hello</h1>';
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+    e.preventDefault();
+
+    if (nameInput.value === '' || emailInput.value === '') {
+        msg.classList.add('error');
+        msg.innerHTML = 'Please enter all fields!';
+
+        setTimeout(() => msg.remove(), 3000);
+
+    } else {
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
+
+        userList.appendChild(li);
+
+        // Clear fields
+        nameInput.value = '';
+        emailInput.value = '';
+    }
+}
